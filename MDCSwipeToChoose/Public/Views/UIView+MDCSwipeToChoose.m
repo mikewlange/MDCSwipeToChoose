400c2f81e78eddbdcd5401264fb0470ac932b70e
typ-Dating <khusboomotwani@typdevteam.com>
2016-09-28 17:59:16 +0530

    Update UIView+MDCSwipeToChoose.m

--- MDCSwipeToChoose/Public/Views/UIView+MDCSwipeToChoose.m
+++ MDCSwipeToChoose/Public/Views/UIView+MDCSwipeToChoose.m
@@ -256,10 +256,6 @@ const void * const MDCViewStateKey = &MDCViewStateKey;
         // the updates via the pan block.
         CGPoint translation = [panGestureRecognizer translationInView:view];
         view.center = MDCCGPointAdd(self.mdc_viewState.originalCenter, translation);
-        // Update the position and transform. Then, notify any listeners of
-        // the updates via the pan block.
-        CGPoint translation = [panGestureRecognizer translationInView:view];
-        view.center = MDCCGPointAdd(self.mdc_viewState.originalCenter, translation);
         if (view.center.x > self.mdc_viewState.originalCenter.x) {
                     //NSLog(@"Right ");
             [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"isCardMovingLeft"];
